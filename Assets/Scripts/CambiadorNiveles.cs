a83627476c5ac558c5bb98d7e8dddca6bdf324e9
BlckShazam <Ylevendnc@gmail.com>
2016-12-20 19:55:14 +0100

    Cambiador niveles
    No completo.

---
+++ Assets/Scripts/CambiadorNiveles.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+                               
+public class CambiadorNiveles : MonoBehaviour {
+
+	GameObject = GameObject.Find("EquipoRosa");
+	GameObject = GameObject.Find("EquipoAzul");
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (contador >= 5) {
+			Application.LoadLevel("EscenarioNave");
+		}
+	}
+
+}
---
+++ Assets/Scripts/PorteriaGoles.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PorteriaGoles : MonoBehaviour {
+	public Text texto;
+
+	public int contador = 0;
+	Text TextoContador;
+	// Use this for initialization
+	void Start () {
+		TextoContador = GetComponent<Text> ();
+
+		TextoContador.text = contador + "ROSAS";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (contador <= 5) {
+			contador++;
+		}
+	
+	}
+}
