00fc2497729e38317c14fa916deefe566d40a00e
BlckShazam <Ylevendnc@gmail.com>
2016-12-20 19:25:44 +0100

    Jugador 2
    Movimiento del jugador 2 en base a las teclas WASD.

---
+++ Assets/Scripts/Player02.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player02 : MonoBehaviour {
+	private Rigidbody2D rb;
+	public float velocidad = 5f;
+	public float fuerza = 10;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			movimientoDerecha ();
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			movimientoIzquierda ();
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			movimientoArriba ();
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			movimientoAbajo ();
+		}
+	}
+	void movimientoDerecha (){
+		rb.velocity = new Vector2(velocidad*fuerza, rb.velocity.y);
+		transform.localScale = new Vector3(1,1,1);
+	} 
+
+	void movimientoIzquierda(){
+		rb.velocity = new Vector2 (-velocidad * fuerza, rb.velocity.x);
+		transform.localScale = new Vector3 (-1, 1, 1);
+	}
+
+	void movimientoArriba (){
+		rb.velocity = new Vector2 (0, velocidad * fuerza);
+		transform.localScale = new Vector3 (1, 1, 1);
+	}
+	void movimientoAbajo (){
+		rb.velocity = new Vector2(0, -velocidad*fuerza);
+		transform.localScale = new Vector3(1,-1,1);
+	}
+}
